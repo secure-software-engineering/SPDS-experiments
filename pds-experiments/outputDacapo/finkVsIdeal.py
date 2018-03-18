8dbd675e445ebfc8d9159ed8d5ea7290ce923e0b
Johannes Spaeth <joh.spaeth@googlemail.com>
2018-03-18 12:58:27 +0100

    Updated of analysis results

--- pds-experiments/outputDacapo/finkVsIdeal.py
+++ pds-experiments/outputDacapo/finkVsIdeal.py
@@ -17,14 +17,14 @@ def beautifyAnalysisTime(time):
     return int(time)
 
 def geo_mean(iterable):
-    return np.mean(iterable)
-    #if len(iterable) == 0:
-    #    return 0;
-    #a = np.log(iterable)
-    #return np.exp(a.sum()/len(a))
+   # return np.mean(iterable)
+    if len(iterable) == 0:
+        return 0;
+    a = np.log(iterable)
+    return np.exp(a.sum()/len(a))
 def getData(filename, replacement):
-    if os.path.isfile(filename.replace("ideal-ap",replacement)):
-        with open(filename.replace("ideal-ap",replacement)) as csvfile:
+    if os.path.isfile(filename.replace("ideal",replacement)):
+        with open(filename.replace("ideal",replacement)) as csvfile:
             readerFINK_UNIQUE = csv.DictReader(csvfile,delimiter = ";")
             return list(readerFINK_UNIQUE)
     return []
@@ -32,11 +32,13 @@ def toSeconds(ms):
     return (ms/1000).round(1)
 
 results ={}
-
 path = "*.csv"
+
+uniqueSeeds = set()
 for fname in glob.glob(path):
     #if "dacapo.bloat.Main2-ideal-ap-IO" in fname:
-    if "ideal-ap" in fname:
+    if "ideal" in fname and not "-ap" in fname and not "_vs_" in fname:
+        #print fname
         timesIDEAL = []
         timesIDEAL_AP = []
         timesFINK_UNIQUE = []
@@ -45,45 +47,64 @@ for fname in glob.glob(path):
         timeoutsIDEAL_AP = 0
         timeoutsFINK_UNIQUE = 0
         timeoutsFINK_APMUST = 0
-        dataIDEAL_AP = getData(fname,"ideal-ap")
+        errorsIDEAL = 0
+        errorsFINK_UNIQUE = 0
+        errorsFINK_APMUST = 0
+       # dataIDEAL_AP = getData(fname,"ideal-ap")
         dataIDEAL =  getData(fname,"ideal")
         dataFINK_UNIQUE = getData(fname,"fink-unique")
         dataFINK_APMUST = getData(fname,"fink-apmust")
-        for rowIDEAL_AP in dataIDEAL_AP:
+        for rowIDEAL_AP in dataIDEAL:
             foundInFINK_UNIQUE = False
             for rowFINK_UNIQUE in dataFINK_UNIQUE:
                 if rowFINK_UNIQUE['SeedMethod'] == rowIDEAL_AP['SeedMethod'] and int(rowFINK_UNIQUE['VisitedMethod']) != 0 and int(rowIDEAL_AP['VisitedMethod']) != 0:
                     if not foundInFINK_UNIQUE:
-                        timesFINK_UNIQUE.append(beautifyAnalysisTime(rowFINK_UNIQUE['AnalysisTimes']))
-                        timesIDEAL_AP.append(beautifyAnalysisTime(rowIDEAL_AP['AnalysisTimes']))
                         if rowIDEAL_AP['Timedout'] == "true":
-                            timeoutsIDEAL_AP += 1
+                            timeoutsIDEAL += 1
+                            timesIDEAL.append(30000)
+                        else:
+                            timesIDEAL.append(beautifyAnalysisTime(rowIDEAL_AP['AnalysisTimes']))
                         if rowFINK_UNIQUE['Timedout'] == "true":
                             timeoutsFINK_UNIQUE += 1
+                            timesFINK_UNIQUE.append(30000)
+                        else:
+                            timesFINK_UNIQUE.append(beautifyAnalysisTime(rowFINK_UNIQUE['AnalysisTimes']))
+                        if rowIDEAL_AP['Is_In_Error'] == "true":
+                            errorsIDEAL += 1
+                            uniqueSeeds.add(rowIDEAL_AP['Seed'])
+                        if rowFINK_UNIQUE['Is_In_Error'] == "true":
+                            errorsFINK_UNIQUE += 1
                     foundInFINK_UNIQUE = True
             if foundInFINK_UNIQUE:
                 foundInFINK_APMUST = False
                 for rowFINK_APMUST in dataFINK_APMUST:                    
                     if rowFINK_APMUST['SeedMethod'] == rowIDEAL_AP['SeedMethod']:
                         if not foundInFINK_APMUST:
-                            timesFINK_APMUST.append(beautifyAnalysisTime(rowFINK_APMUST['AnalysisTimes']))
                             foundInFINK_APMUST = True
                             if rowFINK_APMUST['Timedout'] == "true":
                                 timeoutsFINK_APMUST += 1
-                for rowIDEAL in dataIDEAL:
-                    if (rowIDEAL['SeedMethod'] == rowIDEAL_AP['SeedMethod'] and rowIDEAL['SeedStatement'] == rowIDEAL_AP['SeedStatement']):
-                        timesIDEAL.append(beautifyAnalysisTime(rowIDEAL['AnalysisTimes']))
-                        if rowIDEAL['Timedout'] == "true":
-                            timeoutsIDEAL += 1
+                                timesFINK_APMUST.append(30000)
+                            else:
+                                timesFINK_APMUST.append(beautifyAnalysisTime(rowFINK_APMUST['AnalysisTimes']))
+
+                            if rowFINK_APMUST['Is_In_Error'] == "true":
+                                errorsFINK_APMUST += 1
+               # for rowIDEAL in dataIDEAL:
+                #    if (rowIDEAL['SeedMethod'] == rowIDEAL_AP['SeedMethod'] and rowIDEAL['SeedStatement'] == rowIDEAL_AP['SeedStatement']):
+                 # #      timesIDEAL.append(beautifyAnalysisTime(rowIDEAL['AnalysisTimes']))
+                  #      if rowIDEAL['Timedout'] == "true":
+                    #        timeoutsIDEAL += 1
         if(len(dataIDEAL) < len(timesIDEAL_AP)):
             print "SOMETHING WENT WRONG"
-        if len(timesIDEAL_AP) != 0:
-            print fname.replace("ideal-ap","")
+        if len(timesIDEAL) != 0:
+            print fname.replace("ideal","")
+            print timesIDEAL
             print str(len(timesIDEAL)) + " "+ str(geo_mean(timesIDEAL))
             print str(len(timesIDEAL_AP)) + " "+ str(geo_mean(timesIDEAL_AP))
             print str(len(timesFINK_UNIQUE)) + " "+ str(geo_mean(timesFINK_UNIQUE))
             print str(len(timesFINK_APMUST)) + " "+ str(geo_mean(timesFINK_APMUST))
             outputFileName = ""
+            print "IDEAL Errors:" + str(errorsIDEAL)
             if "-IO.csv" in fname:
                 outputFileName = "io_ideal_vs_fink.csv"
             if "-EmptyVector.csv" in fname:
@@ -93,14 +114,19 @@ for fname in glob.glob(path):
             d = {}
             if outputFileName in results:
                 d = results[outputFileName]
-            entry = {"name":   fname, 
+            benchmarkName = fname.replace("dacapo.","")
+            benchmarkName = benchmarkName[0:benchmarkName.find(".")]
+            entry = {"benchmark":  benchmarkName, 
                     "ideal": toSeconds(geo_mean(timesIDEAL)),
                     "fink": toSeconds(geo_mean(timesFINK_UNIQUE)),
                     "fink_apmust": toSeconds(geo_mean(timesFINK_APMUST)),
-                    "seeds": len(timesIDEAL_AP),
+                    "seeds": len(timesIDEAL),
                     "timeouts_fink": timeoutsFINK_UNIQUE,
                     "timeouts_finkapmust": timeoutsFINK_APMUST,
-                    "timeouts_ideal": timeoutsIDEAL}
+                    "timeouts_ideal": timeoutsIDEAL,
+                    "errors_fink": errorsFINK_UNIQUE,
+                    "errors_finkapmust": errorsFINK_APMUST,
+                    "errors_ideal": errorsIDEAL}
             d[fname] = entry
             results[outputFileName] = d
           #  if results[outputFileName] == None:
@@ -114,7 +140,7 @@ for fname in glob.glob(path):
           #  file.write(fname + ","+ str(num_lines) + ","+str(toSeconds(geo_mean(timesIDEAL)))+","+str(toSeconds(geo_mean(timesFINK_UNIQUE)))+","+str(toSeconds(geo_mean(timesFINK_APMUST)))+"\n")
           #  file.close() 
 
-header = ["name", "ideal","fink","fink_apmust","seeds","timeouts_fink","timeouts_finkapmust","timeouts_ideal"]
+header = ["benchmark", "ideal","fink","fink_apmust","seeds","timeouts_fink","timeouts_finkapmust","timeouts_ideal","errors_fink","errors_finkapmust","errors_ideal"]
 
 for i in results:
     file = open(i,"w")
@@ -129,7 +155,10 @@ for i in results:
         for k in header:
             file.write(str(results[i][j][k]) + ";")
         file.write(str(index)+"\n")
-        print str(results[i][j]["seeds"]) + "&"
         avegareRatiosTime.append(float(results[i][j]["fink_apmust"])/results[i][j]["ideal"])
         index += 1
+    print avegareRatiosTime
     print "Ratio Times: IDEAL vs Fink AP Must:" + str(geo_mean(avegareRatiosTime))
+
+for i in uniqueSeeds:
+    print i
